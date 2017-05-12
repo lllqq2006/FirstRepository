9ac2a4cbc96fccabddee3beb7c071d7fcc2ac144
frzhang <frzhang@microsoft.com>
2017-05-12 17:43:23 +0800

    first acm project.

---
+++ src/ACMProblem/ACMProblem/ACMProblem.cpp
@@ -0,0 +1,14 @@
+// ACMProblem.cpp : Defines the entry point for the console application.
+//
+
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+    return 0;
+}
+
